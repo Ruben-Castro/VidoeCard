4465670b1b4f8e8e154eda8f46ef416b5e7c4ea0
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-06-29 14:22:06 -0700

    Initial Commit

---
+++ VidoeCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VidoeCard
+//
+//  Created by Ruben Castro Espinoza on 6/29/20.
+//  Copyright © 2020 Ruben Castro Espinoza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
